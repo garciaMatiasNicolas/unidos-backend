d763c098e0d4b9db728f7b0c50bd202b39c8f162
Matias Garcia <garciamatias.159@gmail.com>
2023-03-10 10:32:25 -0300

    index

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import route from "./routes/Routes.js";
 import cors from 'cors';
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors({
     credentials:true,
@@ -14,5 +14,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use('/api', route);
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Servidor Unidos</h1>')
+})
 
 app.listen(PORT, console.log('servidor corriendo'));
